ef35a790d43abb384846587a3c31afb11223d900
geraldspreer <geraldspreer@gmail.com>
2022-02-08 14:57:53 +0100

    Add test-helper.ts

---
+++ angular/test-helper.ts
@@ -0,0 +1,46 @@
+import { DebugElement } from "@angular/core";
+import { ComponentFixture } from "@angular/core/testing";
+
+export class TestHelper<T> {
+  fixture: ComponentFixture<T>;
+  debug: DebugElement;
+
+  constructor(fixture: ComponentFixture<T>) {
+    this.fixture = fixture;
+    this.debug = fixture.debugElement;
+  }
+
+  public findElement(selector: string): HTMLElement {
+    return this.fixture.nativeElement.querySelector(selector);
+  }
+  
+  public clickElement(selector: string) {
+    this.debug.nativeElement.querySelector(selector).click();
+    this.fixture.detectChanges();
+  }
+
+  public setInputWithSelectorTo(selector: string, value: string | number) {
+    const inputToSet = this.debug.nativeElement.querySelector(selector);
+    inputToSet.value = value;
+    inputToSet.dispatchEvent(new Event("input"));
+  }
+
+  public changeSelectInputTo(selector: string, index: number) {
+    const selectInput = this.debug.nativeElement.querySelector(selector);
+    selectInput.value = selectInput.options[index].value;
+    selectInput.dispatchEvent(new Event("change"));
+  }
+  
+  public hasElement = (selector: string) {
+    return expect(helper.findElement(selector))
+    .withContext(`${selector} should be visible`)
+    .not.toBeNull();
+  };
+
+  public doesNotHaveElement = (selector: string) {
+    return expect(helper.findElement(selector))
+    .withContext(`${selector} should not be visible`)
+    .toBeNull();
+  };
+
+}
